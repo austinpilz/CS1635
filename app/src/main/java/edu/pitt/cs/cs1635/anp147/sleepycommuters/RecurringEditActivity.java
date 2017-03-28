6d9edafb278f9eeb3f14c6f1dd8e321ebddfb150
austinpilz <austinpilz@gmail.com>
2017-03-28 01:25:00 -0400

    Adding recurring edit activity

---
+++ app/src/main/java/edu/pitt/cs/cs1635/anp147/sleepycommuters/RecurringEditActivity.java
@@ -0,0 +1,13 @@
+package edu.pitt.cs.cs1635.anp147.sleepycommuters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecurringEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recurring_edit);
+    }
+}
