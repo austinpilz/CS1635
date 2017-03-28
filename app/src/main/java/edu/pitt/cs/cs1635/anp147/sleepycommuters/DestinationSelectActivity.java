56e675423dd2343f6f65f46cf57a319063743816
austinpilz <austinpilz@gmail.com>
2017-03-28 02:35:43 -0400

    Added destination fields

--- app/src/main/java/edu/pitt/cs/cs1635/anp147/sleepycommuters/DestinationSelectActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/anp147/sleepycommuters/DestinationSelectActivity.java
@@ -1,7 +1,12 @@
 package edu.pitt.cs.cs1635.anp147.sleepycommuters;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class DestinationSelectActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class DestinationSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination_select);
+
+        final Intent intent = getIntent();
+        TextView textView = (TextView) findViewById(R.id.destinationHeadText);
+        textView.setText("Showing destination stops for the " + intent.getStringExtra("line") + " originating at "+  intent.getStringExtra("stopName"));
+
+        LinearLayout mLinearLayout = (LinearLayout) findViewById(R.id.destinationLV);
+
+        final RadioButton[] rb = new RadioButton[5];
+        RadioGroup rg = new RadioGroup(this);
+        rg.setOrientation(RadioGroup.VERTICAL);
+        for (int i = 1; i < 5; i++) {
+            rb[i] = new RadioButton(this);
+            rg.addView(rb[i]);
+            rb[i].setText("Destination Stop " + i);
+
+        }
+        mLinearLayout.addView(rg);
+
+
     }
 }
